aa00904797d9688c792c9b1920adcdbd082c8d3e
ArindamDongre <arindamdongre2253@gmail.com>
2023-07-18 18:22:29 +0530

    q

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import express from "express";
-import cors from "cors";
-import { createServer } from "http";
-import { Server } from "socket.io";
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
 const app = express();
-const server = createServer(app);
+const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "*",
