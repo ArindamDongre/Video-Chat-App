23866aa0b38a03128a89d99505bd99d5364395c8
ArindamDongre <arindamdongre2253@gmail.com>
2023-07-18 21:10:51 +0530

    extremely final

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from "express";
 import cors from "cors";
-import { createServer } from "http";
+import http from "http";
 import { Server } from "socket.io";
 const app = express();
-const server = createServer(app);
+const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "*",
